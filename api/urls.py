80638a51e086cc75b438b20d7915ed9d5401a0c0
donfortune <donfortunet.df@gmail.com>
2024-05-16 13:50:51 +0100

    handled cors errors

--- api/urls.py
+++ api/urls.py
@@ -3,4 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.getRoutes, name="routes"),
+    path('notes/', views.getNotes),
+    path('note/<int:id>/', views.getNote),
+    path('notes/create/', views.createNote),
+    path('notes/<int:id>/update', views.updateNote),
+    path('notes/<int:id>/delete', views.deleteNote)
     ]
--- api/views.py
+++ api/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from .serializers import NoteSerializer
+from .models import Note
 
 # Create your views here.
 @api_view(['GET'])
@@ -39,3 +41,51 @@ def getRoutes(request):
         
     ]
     return Response(routes)
+
+@api_view(['GET'])
+def getNotes(request):
+    notes = Note.objects.all()
+    serializer = NoteSerializer(notes, many=True)
+    #context = {'Notes': notes}
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+def getNote(request, id):
+    note = Note.objects.get(id=id)
+    serializer = NoteSerializer(note)
+    return Response(serializer.data)
+
+@api_view(['POST'])
+def createNote(request):
+    data = request.data
+    note = Note.objects.create(
+        body=data['body']
+    )
+
+    serializer = NoteSerializer(note)
+    return Response(serializer.data)
+
+
+@api_view(['PUT'])
+def updateNote(request, id):
+    data = request.data
+    note = Note.objects.get(id=id)
+    serializer = NoteSerializer(note, data)
+    if serializer.is_valid():
+        serializer.save()
+    return Response(serializer.data)
+
+
+@api_view(['DELETE'])
+def deleteNote(request, id):
+    #data = request.data
+    note = Note.objects.get(id=id)
+    note.delete()
+    return Response('Note was deleted!')
+
+
+
+
+
+
